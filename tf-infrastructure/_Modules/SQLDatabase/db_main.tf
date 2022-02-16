6f1ad05eda012f67d8012ce2d662158db8a71bb2
asebetich <78879975+asebetich@users.noreply.github.com>
2022-02-16 15:02:22 -0800

    Update db_main.tf

--- tf-infrastructure/_Modules/SQLDatabase/db_main.tf
+++ tf-infrastructure/_Modules/SQLDatabase/db_main.tf
@@ -41,4 +41,5 @@ resource "azurerm_sql_database" "db" {
 output "connection_string" {
   description = "Connection string for the Azure SQL Database created."
   value       = "Server=tcp:${azurerm_sql_server.dbserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_database.db.name};Persist Security Info=False;User ID=${azurerm_sql_server.dbserver.administrator_login};Password=${azurerm_sql_server.dbserver.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  sensitive   = true 
 }
